073718be6acba869bd10bf8af4b9586dd069dbc5
petermousses <peter.mousses@gmail.com>
2021-02-26 12:42:34 -0700

    Swapped code

--- GitDemo.java
+++ GitDemo.java
@@ -2,13 +2,13 @@
  * Edit this line to have your name.
  ******/
 public class GitDemo {
-    
+
     private double previousNumber;
-    
+
     public GitDemo() {
         previousNumber = 1;
     }
-    
+
     /*****
      * This function should add 'num' to 'previousNumber' and return the sum.
     *****/
@@ -16,37 +16,38 @@ public class GitDemo {
         System.out.print("Add num to previousNumber");
         return 0;
     }
-    
+
     /*****
      * This function should calculate the factorial of 'num' and return the resulting value.
      * i.e. if num = 4,
      *  the function will return the value of (1 * 2 * 3 * 4)
      *****/
     public double findFactorial(int num) {
-        double factorialNumber = 0;
-        
-        while( num != 0 ) {
-            double temp = num % 10;
-            factorialNumber = factorialNumber * 10 + temp;
-            num /= 10;
+
+        double newNumber = 1;
+
+        for(int i = 1; i <= num; ++i){
+            newNumber = newNumber * i;
         }
-        
-        return factorialNumber;
+
+        return newNumber;
     }
-    
+
     /*****
      * This function should reverse 'num' and return the resulting value.
      * i.e. if num = 1234
      *  the function will return 4321
      *****/
     public double reverseNumber(int num){
-        double newNumber = 1;
-        
-        for(int i = 1; i <= num; ++i){
-            newNumber = newNumber * i;
-        }
-        
-        return newNumber;
+      double factorialNumber = 0;
+
+      while( num != 0 ) {
+          double temp = num % 10;
+          factorialNumber = factorialNumber * 10 + temp;
+          num /= 10;
+      }
+
+      return factorialNumber;
     }
-    
+
 }
